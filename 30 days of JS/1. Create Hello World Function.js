7962cd918d91badafa07563736af1ea66d743bb4
ITboo <annaacdc@tut.by>
2023-10-09 19:35:31 +0300

    add 1-2

---
+++ 30 days of JS/1. Create Hello World Function.js
@@ -0,0 +1,5 @@
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+const createHelloWorld = function() {
+    return (...args) => "Hello World";
+};
